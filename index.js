37b5ed9c4d2ddc3f00b6ace938bb6f10cc8f29bb
RH <reganhwang@protonmail.com>
2024-12-13 00:50:33 +0900

    New time calculation working. Not tested yet.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 //https://codepen.io/afarrar/pen/JRaEjP
 
 const showTime = ()=>{
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
+    let date = new Date();
+    let dayOfWeek = date.getDay();
+    let hr = date.getHours(); // 0 - 23
+    let min = date.getMinutes(); // 0 - 59
+    let sec = date.getSeconds(); // 0 - 59
+    if(dayOfWeek===0){
+        dayOfWeek = 7;
+    }
+    let daysGone = dayOfWeek-1;
+    let hoursGone = hr + 24*daysGone;
+    let day = (hoursGone / 28) | 0;
+    hr = hoursGone %28;
+
+    /*
     var session = "AM";
     
     if(h == 0){
@@ -19,13 +29,14 @@ const showTime = ()=>{
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
+    */
+    let time = hr + ":" + min + ":" + sec;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     
     setTimeout(showTime, 1000);
     
 }
+
 
 showTime();
