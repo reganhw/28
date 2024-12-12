e9b2d18bc2106d2d04056933242e7c6d92ce20d2
RH <reganhwang@protonmail.com>
2024-12-13 00:58:04 +0900

    Day of week working.

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const showTime = ()=>{
     s = (s < 10) ? "0" + s : s;
     */
     let time = hr + ":" + min + ":" + sec;
-    document.getElementById("clock").innerText = time;
+    //document.getElementById("clock").innerText = time;
+    document.getElementById("day").textContent = `Day ${day} of the week`
     document.getElementById("clock").textContent = time;
     
     setTimeout(showTime, 1000);
