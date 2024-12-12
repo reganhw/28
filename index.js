cc007ca8f0de8694e5a60a48fd31ebb41f25eafa
RH <reganhwang@protonmail.com>
2024-12-13 00:28:25 +0900

    Changed function to arrow function.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function showTime(){
+//https://codepen.io/afarrar/pen/JRaEjP
+
+const showTime = ()=>{
     var date = new Date();
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
